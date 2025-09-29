b37296625afca6e2fcfe6c6c09d052123f7eda98
spiers448 <spiers-bs@ulster.ac.uk>
2025-09-29 15:13:39 +0100

    added code

--- Week2-Methods/Program.cs
+++ Week2-Methods/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Windows.Markup;
+
+CreateMenu();
+
+void CreateMenu()
+{
+    try
+    {
+        //draw menu
+        Console.WriteLine("--Main Menu--");
+        Console.WriteLine($"1 - Say hello\n2 - Add numbers\n3 - Calculate area\n------------\nChoose an option");
+
+        //accept users choice
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                SayHello();
+            break;
+            case "2":
+                AddNumbers();
+            break;
+            case "3":
+                CalculateArea();
+            break;
+
+            default:
+                Console.WriteLine("Invalid choice");
+            break;
+        }
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
+
+static void SayHello()
+{
+    Console.WriteLine("Hello Word!");
+}
+
+static void AddNumbers()
+{
+    try
+    {
+        Console.WriteLine("Enter number of values to be added");
+        int amount = Convert.ToInt16(Console.ReadLine());
+        int value = 0;
+        for (int i = 0; i < amount; i++)
+        {
+            Console.WriteLine($"enter value {i}");
+            value += Convert.ToInt16(Console.ReadLine());
+        }
+        Console.WriteLine($"Answer is {value}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
+
+static void CalculateArea()
+{
+    try
+    {
+        //get user input
+        Console.WriteLine("Enter length");
+        double length = Convert.ToInt64(Console.ReadLine());
+
+        Console.WriteLine("Enter width");
+        double width = Convert.ToInt64(Console.ReadLine());
+
+        //calculate area
+        double area = length * width;
+        Console.WriteLine($"Area is {area}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+
+}
